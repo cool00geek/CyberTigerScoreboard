ebe0594d4447c1f488401a181a9c62c82f01fbdc
Vinay Venkat <goldenvinay4@gmail.com>
2017-03-13 12:23:29 -0700

    C# and heavy jar

--- Clients/C# DB Sender/CSDB/Program.cs
+++ Clients/C# DB Sender/CSDB/Program.cs
@@ -9,8 +9,32 @@ namespace CSDB
     {
         static void Main(string[] args)
         {
-            String username = "";
-            String password = "";
+            String username;
+            String password;
+            String team;
+            int score;
+            int time;
+            bool quiet;
+
+            try
+            {
+                username = args[0];
+                password = args[1];
+                team = args[2];
+                score = int.Parse(args[3]);
+                time = int.Parse(args[4]);
+                quiet = Convert.ToBoolean(args[5]);
+            }
+            catch (Exception e)
+            {
+                username = "";
+                password = "";
+                team = "";
+                score = 0;
+                time = 0;
+                quiet = false;
+            }
+
             string connectionString = "Server = tcp:ctsb.database.windows.net,1433; Initial Catalog = CPscores; Persist Security Info = False; User ID = " + username + "; Password =" + password + "; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
             string cmd = "INSERT INTO TeamScores (TeamName,Score,Time) VALUES (@val1, @val2, @val3)";
             using (QC.SqlConnection conn = new QC.SqlConnection(connectionString))
@@ -18,29 +42,40 @@ namespace CSDB
                 using (QC.SqlCommand command = new QC.SqlCommand(cmd))
                 {
                     command.Connection = conn;
-                    //command.CommandString = cmd;
 
-                    command.Parameters.AddWithValue("@val1", "Matrix");
-                    command.Parameters.AddWithValue("@val2", 9000);
-                    command.Parameters.AddWithValue("@val3", 80);
+                    command.Parameters.AddWithValue("@val1", team);
+                    command.Parameters.AddWithValue("@val2", score);
+                    command.Parameters.AddWithValue("@val3", time);
 
                     try
                     {
-                        Console.WriteLine("Opening connection...");
+                        if (!quiet)
+                        {
+                            Console.WriteLine("Opening connection...");
+                        }
                         conn.Open();
-                        Console.WriteLine("Transmitting data!");
+                        if (!quiet)
+                        {
+                            Console.WriteLine("Transmitting data!");
+                        }
                         command.ExecuteNonQuery();
-                        Console.WriteLine("Done!");
+                        if (!quiet)
+                        {
+                            Console.WriteLine("Done!");
+                        }
                     }
-                    catch(QC.SqlException e)
+                    catch (QC.SqlException e)
                     {
                         Console.WriteLine("An error has occured");
                         Console.WriteLine(e);
                     }
                 }
             }
-            Console.Write("Press any key to continue...");
-            Console.ReadKey(true);
+            if (!quiet)
+            {
+                Console.Write("Press any key to continue...");
+                Console.ReadKey(true);
+            }
         }
     }
 }
